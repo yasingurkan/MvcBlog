154c26ab46070649a880e6588ce2af2552ca5dcc
Yasin GURKAN <yasingurkann@gmail.com>
2018-08-05 15:05:03 +0300

    Initial Commit

--- MvcBlog/Controllers/HomeController.cs
+++ MvcBlog/Controllers/HomeController.cs
@@ -8,22 +8,13 @@ namespace MvcBlog.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult CategoryWidgetGetir()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
